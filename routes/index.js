9bebc643267e8e3c165605e8927d0c24f66b0513
kenu <kenu.heo@gmail.com>
2020-06-03 17:48:54 +0900

    okdevtv-list

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var sheetdb = require('../lib/sheetdb');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   console.log(req.session.user);
   res.render('index', { user: req.session.user });
 });
 
+router.get('/okdevtv-list', async function (req, res) {
+  const info = { sheetId: process.env.SHEET_ID, index: 1 };
+  const sheet = await sheetdb.getSheet(info);
+  const rows = await sheet.getRows();
+  console.log(rows.length);
+  const items = rows.map((row) => {
+    return row;
+  });
+
+  res.render('okdevtv-list', { title: 'OKdevTV List', items: items });
+});
 module.exports = router;
