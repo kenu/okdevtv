bfbaa2ad3844c384077bf5f3e2b1d62c35fbfdca
kenu <kenu.heo@gmail.com>
2021-02-11 07:48:32 +0900

    link

--- public/md/elk/elastic-setup.sh
+++ public/md/elk/elastic-setup.sh
@@ -29,7 +29,7 @@ nohup bin/kibana &
 cd ~/local
 wget https://artifacts.elastic.co/downloads/logstash/logstash-7.11.0-linux-x86_64.tar.gz
 tar xfz logstash-7.11.0-linux-x86_64.tar.gz
-ln -s logstash-7.11.0-linux-x86_64 logstash
+ln -s logstash-7.11.0 logstash
 cd logstash
 ./bin/logstash-plugin install logstash-input-beats
 mkdir logconf && cd logconf
