6eeca684e27d66bf8147b76a6f501ce1ca162ad4
kenu <kenu.heo@gmail.com>
2021-04-08 11:44:20 +0900

    deploy noti

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,6 @@ npm install
 
 pm2 restart all
 
+# curl -X POST -H 'Content-type: application/json' --data '{"text":"Deployment Finished!"}' https://hooks.slack.com/services/xxxxR3xx4/B0xxDNExx11/xxxWeAVFWcFJOoJBxxxx6Bxx
+
 # end
