3b65811da7d1717504c7e380e41a8c6898675e5b
kenu <kenu@okjsp.pe.kr>
2021-10-15 07:52:08 +0900

    7.15.1

--- public/md/elk/elastic-setup.sh
+++ public/md/elk/elastic-setup.sh
@@ -9,23 +9,23 @@ sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
 mkdir ~/local
 cd ~/local
 rm -rf elasticsearch kibana logstash
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.15.0-linux-x86_64.tar.gz
-tar xfz elasticsearch-7.15.0-linux-x86_64.tar.gz
-ln -s elasticsearch-7.15.0 elasticsearch
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.15.1-linux-x86_64.tar.gz
+tar xfz elasticsearch-7.15.1-linux-x86_64.tar.gz
+ln -s elasticsearch-7.15.1 elasticsearch
 cd elasticsearch
 bin/elasticsearch -d
 
 cd ~/local
-wget https://artifacts.elastic.co/downloads/kibana/kibana-7.15.0-linux-x86_64.tar.gz
-tar xfz kibana-7.15.0-linux-x86_64.tar.gz
-ln -s kibana-7.15.0-linux-x86_64 kibana
+wget https://artifacts.elastic.co/downloads/kibana/kibana-7.15.1-linux-x86_64.tar.gz
+tar xfz kibana-7.15.1-linux-x86_64.tar.gz
+ln -s kibana-7.15.1-linux-x86_64 kibana
 cd kibana
 nohup bin/kibana &
 
 cd ~/local
-wget https://artifacts.elastic.co/downloads/logstash/logstash-7.15.0-linux-x86_64.tar.gz
-tar xfz logstash-7.15.0-linux-x86_64.tar.gz
-ln -s logstash-7.15.0 logstash
+wget https://artifacts.elastic.co/downloads/logstash/logstash-7.15.1-linux-x86_64.tar.gz
+tar xfz logstash-7.15.1-linux-x86_64.tar.gz
+ln -s logstash-7.15.1 logstash
 cd logstash
 ./bin/logstash-plugin install logstash-input-beats
 mkdir logconf && cd logconf
