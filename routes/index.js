c60f1500cc835a2df5c03c8ec5317910ed448b0c
kenu <kenu.heo@gmail.com>
2023-01-12 14:19:18 +0900

    645 lotto ⭐️

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,57 @@ const okdevtv = require('../services/okdevtv-list');
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { user: req.session.user });
+  res.render('index', { user: req.session.user });
 });
 
 router.get('/okdevtv-list', async function (req, res) {
-    const items = await okdevtv.getItems(req);
+  const items = await okdevtv.getItems(req);
 
-    res.render('okdevtv-list', { title: '<a href="/">OKdevTV List</a>', desc: 'OKdevTV', items: items });
+  res.render('okdevtv-list', {
+    title: '<a href="/">OKdevTV List</a>',
+    desc: 'OKdevTV',
+    items: items,
+  });
 });
 module.exports = router;
 
 router.get('/f/:url', function (req, res) {
-    const url = req.params.url;
-    res.redirect(url);
+  const url = req.params.url;
+  res.redirect(url);
 });
+
+router.get('/645', function (req, res) {
+  function getRandomList(max, count) {
+    const list = [];
+    for (let i = 0; i < max; i++) {
+      list.push(i + 1);
+    }
+    ArrayUtils.shuffle(list);
+    return list.slice(0, count);
+  }
+  res.end(getRandomList(45, 6).sort((a, b) => a - b).join(', '));
+});
+
+const ArrayUtils = {
+  shuffle: function (array) {
+    // Knuth suffle
+    // https://stackoverflow.com/a/2450976
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  },
+};
