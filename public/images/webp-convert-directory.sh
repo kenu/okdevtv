3908b1b9c810e25ed7b342921ea3f8f5f9f0e337
kenu <kenu.heo@gmail.com>
2023-02-15 00:56:08 +0900

    ♻️ all images to webp #78

---
+++ public/images/webp-convert-directory.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PARAMS=('-m 6 -q 70 -mt -af -progress')
+BASEDIR=$(dirname "$0")
+for D in `find . -type d`
+do
+  cd $BASEDIR
+  cd $D
+  pwd
+
+  for FILE in *.{jpg,jpeg,png,svg,tif,tiff}; do
+    [ -e "$FILE" ] || continue
+    # Here "$FILE" exists
+    cwebp $PARAMS "$FILE" -o "${FILE%.*}".webp;
+  done
+
+  cd -
+done
