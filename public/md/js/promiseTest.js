29bc06ad25050e9337b3ebdd74b6055508534fb1
kenu <kenu.heo@gmail.com>
2023-10-31 17:19:18 +0900

    var 2 let

--- public/md/js/promiseTest.js
+++ public/md/js/promiseTest.js
@@ -1,22 +1,27 @@
 function testFunction() {
-    return new Promise(function (resolve, reject) {
-        resolve("test1", "test2");
-    });
+  return new Promise(function (resolve, reject) {
+    resolve('test1', 'test2');
+  });
 }
 
 function run() {
-    testFunction()
-        .then(function (e) { console.log(arguments); })
-        .catch(function (err) { console.log(err); });
+  testFunction()
+    .then(function (e) {
+      console.log(arguments);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
 
-async function run() {
-    try {
-        var response = await testFunction();
-        console.log(response); // test1
-    } catch (err) {
-        console.log(err);
-    }
+async function run2() {
+  try {
+    const response = await testFunction();
+    console.log(response); // test1
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 run();
+run2();
