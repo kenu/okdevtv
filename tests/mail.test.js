4fdcc4d8908d847bfbccfaea3a7ea6a671e35b81
kenu <kenu@okjsp.pe.kr>
2024-12-02 22:05:07 +0900

    unused tests

--- tests/mail.test.js
+++
@@ -1,7 +0,0 @@
-const mail = require('../lib/mail')
-
-test('mail test', async () => {
-  const email = process.env.BASE_MAIL
-  const message = 'test message'
-  await mail.send(email, message)
-})
--- tests/simple.test.js
+++
@@ -1,7 +0,0 @@
-const sendToSlack = require('../lib/ajax')
-
-test('axios', async () => {
-  const text = 'Hello world!'
-  const result = await sendToSlack(text + ' ' + Date.now())
-  expect(result).toEqual('ok')
-})
